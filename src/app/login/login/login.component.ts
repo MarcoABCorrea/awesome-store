03506da972964f9fa92a0a46129b6de360c4c521
Marco Correa <marcoabcorrea@gmail.com>
2018-04-11 22:08:02 -0300

    AWE-3 - Adicionando serviço que recupera todos os dados do usuário

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {AuthService} from "../../auth.service";
 import LoggedUser from "../../models/logged-user.model";
 import {default as swal} from "sweetalert2";
+import {environment} from "../../../environments/environment";
+import {Headers, Http} from "@angular/http";
 
 @Component({
 	selector: 'app-login',
@@ -16,7 +18,7 @@ export class LoginComponent {
 		password: null
 	};
 
-	constructor(private authService: AuthService) {
+	constructor(private authService: AuthService, private http: Http) {
 	}
 
 	signin() {
@@ -27,15 +29,29 @@ export class LoginComponent {
 		} else {
 			this.authService.sendCredentials(this.login.username, this.login.password)
 				.subscribe(success => {
-					const token = success.json().token;
-					const user: LoggedUser = new LoggedUser(this.login.username, token);
-					this.authService.setLoggedUser(user);
-					location.href = '/home';
+					this.getCompleteUser(success.json().token);
 				}, error => {
 					if (error.status === 401) {
 						swal('Acesso negado!', 'Login e/ou senha inválidos. Tente novamente.', 'error');
 					}
 				});
 		}
+	}
+
+	/**
+	 * Purpose: Recupera o objeto usuário utilizando o email e salva os dados deste no localStorage
+	 * @param {string} token
+	 */
+	getCompleteUser(token: string) {
+		let header: Headers = new Headers();
+		header.append('Authorization', 'Bearer ' + token);
+		this.http.get(`${environment.api}/users?email=${this.login.username}`, {
+			headers: header
+		}).subscribe(success => {
+			const user: any = success.json()[0];
+			const logged: LoggedUser = new LoggedUser(user.name, token, user.roles);
+			this.authService.setLoggedUser(logged);
+			location.href = '/home';
+		})
 	}
 }
--- src/app/models/logged-user.model.ts
+++ src/app/models/logged-user.model.ts
@@ -1,9 +1,13 @@
+import {Roles} from "../enum/roles.enum";
+
 export default class LoggedUser {
 	name: string;
 	token: string;
+	roles: Roles[];
 
-	constructor(name = '', token = '') {
+	constructor(name = '', token = '', roles = []) {
 		this.name = name;
 		this.token = token;
+		this.roles = roles;
 	}
 }
